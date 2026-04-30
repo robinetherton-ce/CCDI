f11cc5ee480d87bfd1647ac0854a313c5a920a43
robinetherton-ce <robinetherton@gmail.com>
2026-04-30 18:15:13 +0200

    Update App.jsx
    -logo update

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,7 @@ function App() {
 
   return (
     <section>
+      <img src="!_assets/CCDI_logo.png" alt="logo" />
       <h1>Counterfeit Currency & Document Index</h1>
 
       <section style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', gap: '1rem' }}>
